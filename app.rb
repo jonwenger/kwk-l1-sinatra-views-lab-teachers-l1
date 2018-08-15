31e9891c95281d0e9a5f231bad51ecb74bb7fa2d
Jon Wenger <jonwenger783@gmail.com>
2018-08-15 12:59:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
-	end
-
-
+  get '/' do
+    erb :fortune
+  end
+  
+  post '/tell_fortune' do
+    @name = params[:name]
+    @sign = params[:sign]
+    erb :results
+  end 
+  
+  get '/hello' do
+    erb :hello
+  end
+  
+  get '/goodbye' do
+    erb :goodbye
+  end
+  
+  
 end
